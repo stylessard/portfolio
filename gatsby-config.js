0de489209a441b96c132d616a8bcabed0a45bf35
Storm Lessard <stormlessard@gmail.com>
2020-11-17 17:10:04 -0500

    updated config favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ty Lessard`,
+        short_name: `Ty Lessard`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/stormlessard-logo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
