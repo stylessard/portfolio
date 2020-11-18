aca8447a18a8e87179800c4601e7e12325d27944
Storm Lessard <stormlessard@gmail.com>
2020-11-18 13:08:44 -0500

    updated project link styles

--- src/components/block.js
+++ src/components/block.js
@@ -3,6 +3,12 @@ import BlockStyles from './block.module.css'
 
 const Block = ({ title, children }) => {
     return (
+        // <div className={BlockStyles.block}>
+        //     <h2>{title}</h2>
+        //     <div className={BlockStyles.blockList}>
+        //         {children}
+        //     </div>
+        // </div>
         <div className={BlockStyles.block}>
             <h2>{title}</h2>
             <div className={BlockStyles.blockList}>
